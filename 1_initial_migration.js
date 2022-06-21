098deb47f5f0a768ba7822fd7dea3b2241af0fca
Matteo D'onofrio <69981848+matteo-Donofrio-unipi@users.noreply.github.com>
2022-06-21 20:01:20 +0200

    Delete 1_initial_migration.js

--- 1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-const Migrations = artifacts.require("Migrations");
-const Contract = artifacts.require("DAppContract");
-
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
-  deployer.deploy(Contract)
-};
